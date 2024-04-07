0f73dac6187e489582f90c2b3ca2060f158ba2f6
Elif Bilge Parlak <elf.prlk64@gmail.com>
2024-04-07 10:23:37 +0300

    Initial Commit

---
+++ RickAndMortyWebMining/RickAndMortyWebMiningApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyWebMiningApp.swift
+//  RickAndMortyWebMining
+//
+//  Created by Elif Parlak on 7.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyWebMiningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
